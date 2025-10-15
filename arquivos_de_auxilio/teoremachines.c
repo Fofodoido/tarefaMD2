b7a05c9fe245f9fe1bd2759a012825cb20028aa7
Fofodoido <rodrigohenriquedonato@gmail.com>
2025-10-15 20:22:23 -0300

    Código inicial do teorema Chinês do Resto

--- arquivos_de_auxilio/teoremachines.c
+++ arquivos_de_auxilio/teoremachines.c
@@ -1,26 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int normalizacao(int x,int y){
-    if (x%){
-        
+long int mod(long int a, long int b) {
+    //implementa de maneira simples o calculo de mod com valores positivos ou negativos
+    if (b < 0) {
+        b = -b;
     }
-    
+    long int r = a % b;
+    if (r < 0) {
+        return r + b;
+    } else {
+        return r;
+    }
 }
 
-int mdc(int n, int m){
-    n=abs(n);
-    m=abs(m);
-    int d,r=n%m;
-    if (n<m){
-        return mdc(m,n);
+long int euclidesExt(long int a, long int b, long int *x, long int *y) {
+    if (b == 0) {
+        *x = 1;
+        *y = 0;
+        return a;
     }
-    
-    while (r!=0){
-        d=r;
-        r=m%r;
-        m=d;
+    long int x1, y1;
+    long int d = euclidesExt(b, a % b, &x1, &y1); //usa de modo recursivo a função até encontrar o caso base, retornando assim os coeficientes
+    *x = y1;
+    *y = x1 - y1 * (a / b);
+    return d;
+}
+
+long int normalizacao(long int x, long int y){
+    //código que normaliza os coeficientes da equação inicial do teorema chinês
+    printf("executando o algoritmo de euclides extendido e normalizando as equacoes.\n");
+    long int cx,cy; // coeficientes que acompanham x e y ao final da execução do algoritmo extendido
+    long int numero = euclidesExt(x,y,&cx,&cy);
+    if (numero!=1){
+        return -1;
     }
+    cx = mod(cx,y);
+    return cx;
+}
+
+long int mdc(long int n, long int m){
+    n = abs(n);
+    m = abs(m);
+    if (n < m){
+        return mdc(m, n);
+    }
+    printf("Iniciando mdc(%ld, %ld)\n", n, m);
+    long int d, r = n % m;
+    printf("Passo: %ld mod %ld = %ld\n", n, m, r);
+    while (r != 0){
+        d = r;
+        r = m % r;
+        m = d;
+    }
+    printf("MDC encontrado: %ld\n", m);
     return m;
 }
 
@@ -34,7 +67,10 @@ int main(){
     scanf("%ld %ld %ld",&n1,&n2,&n3);
     if (mdc(a,n1)==1 && mdc(c,n2)==1 && mdc(e,n3)==1 && mdc(n1,n2)==1 && mdc(n1,n3)==1 && mdc(n2,n3)==1){
         int N=n1*n2*n3;
-        
+        b = mod(normalizacao(a,n1)*b,n1);
+        d = mod(normalizacao(c,n2)*d,n2);
+        f = mod(normalizacao(e,n3)*f,n3);
+        printf("Equacoes normalizadas:\n \n x=%ld mod %ld;\n x=%ld mod %ld;\n x=%ld mod %ld;\n",b,n1,d,n2,f,n3);
     }else{
         printf("O teorema chines nao pode ser aplicado");
         return 0;
